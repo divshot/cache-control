abf39cd3d2cf2915993f0fcf746f1d351d8f9fa3
Scott Corgan <scottcorgan@gmail.com>
2014-09-18 14:33:00 -0700

    Use on-headers module to set cache headers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var minimatch = require('minimatch');
 var slash = require('slasher');
 var url = require('fast-url-parser');
 var regular = require('regular');
+var onHeaders = require('on-headers');
 
 module.exports = function (options) {
   options = options || {};
@@ -12,21 +13,23 @@ module.exports = function (options) {
     var pathname = url.parse(req.url).pathname;
     var matched = false;
     var _rel = slash(pathname);
-
-    _.each(Object.keys(options), function (globKey) {
-      var _glob = slash(globKey);
-     
-      if (minimatch(_rel, _glob) && !matched) {
-        var val = options[globKey];
-        matched = true;
+    
+    onHeaders(res, function () {
+      res.setHeader('Cache-Control', 'public, max-age=300');
+      
+      _.each(Object.keys(options), function (globKey) {
+        var _glob = slash(globKey);
        
-        if (val === false) res.setHeader('Cache-Control', 'no-cache');
-        else if (isNumber(val)) res.setHeader('Cache-Control', 'public, max-age=' + val.toString());
-        else if (_.isString(val)) res.setHeader('Cache-Control', val);
-      }
+        if (minimatch(_rel, _glob) && !matched) {
+          var val = options[globKey];
+          matched = true;
+         
+          if (val === false) res.setHeader('Cache-Control', 'no-cache');
+          else if (isNumber(val)) res.setHeader('Cache-Control', 'public, max-age=' + val.toString());
+          else if (_.isString(val)) res.setHeader('Cache-Control', val);
+        }
+      });
     });
-
-    if (!matched) res.setHeader('Cache-Control', 'public, max-age=300');
     
     next();
     
