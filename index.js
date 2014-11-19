3d54b871b41b79fca68bf014e5f95d6909f4a15d
Scott Corgan <scottcorgan@gmail.com>
2014-11-19 08:31:00 -0800

    refactor: use cache-header module

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 var url = require('fast-url-parser');
 var regular = require('regular');
 var onHeaders = require('on-headers');
-var _isNumber = require('lodash.isnumber');
 var globject = require('globject');
 var slasher = require('glob-slasher');
+var setCacheHeader = require('cache-header');
 
 module.exports = function (cachePaths) {
-  
-  cachePaths = cachePaths || {};
   
   return function (req, res, next) {
     
     var pathname = url.parse(req.url).pathname;
-    var cacheValues = globject(slasher(cachePaths, {value: false}));
+    var cacheValues = globject(slasher(cachePaths || {}, {value: false}));
     var cacheValue = cacheValues(slasher(pathname));
     
     onHeaders(res, function () {
       
+      // Default value
       res.setHeader('Cache-Control', 'public, max-age=300');
-      
-      if (cacheValue === false) {
-        res.setHeader('Cache-Control', 'no-cache');
-      }
-      else if (isNumber(cacheValue)) {
-        res.setHeader('Cache-Control', 'public, max-age=' + cacheValue.toString());
-      }
-      else if (typeof cacheValue === 'string') {
-        res.setHeader('Cache-Control', cacheValue);
-      }
+      setCacheHeader(res, cacheValue);
     });
     
     next();
   };
 };
-
-function isNumber (val) {
-  
-  return _isNumber(val) || regular.number.test(val);
-}
